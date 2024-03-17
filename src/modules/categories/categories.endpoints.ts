1eac6fad80a22d6d6ff0e14ee7854d340bd541e6
eparisLR <emilien.paris348@gmail.com>
2024-03-17 19:17:28 +0100

    fix: erreurs dans la CI

---
+++ src/modules/categories/categories.endpoints.ts
@@ -0,0 +1,4 @@
+export const CategoriesEndpoints = Object.freeze({
+  getAllCategories:
+    process.env.NEXT_PUBLIC_BASE_RECIPES_API_URL + "/categories",
+});
--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,13 +1,11 @@
+import { CategoriesEndpoints } from "./categories.endpoints";
 import { ICategory } from "./categories.type";
 
 export const getAllCategories = async () => {
-  return fetch(
-    `${await process.env.NEXT_PUBLIC_BASE_RECIPES_API_URL}/categories`,
-    {
-      method: "GET",
-      headers: { "Content-type": "application/json" },
-    }
-  )
+  return fetch(`${CategoriesEndpoints.getAllCategories}/categories`, {
+    method: "GET",
+    headers: { "Content-type": "application/json" },
+  })
     .then(async (results) => {
       return results.json().then((categories) => categories as ICategory[]);
     })
