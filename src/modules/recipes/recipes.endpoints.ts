7283e85e528cfe1176a11e351343d6d3c7de6af1
eparisLR <89915713+eparisLR@users.noreply.github.com>
2024-06-26 01:36:30 +0200

    Modification génération URLs

--- src/modules/recipes/recipes.endpoints.ts
+++ src/modules/recipes/recipes.endpoints.ts
@@ -1,7 +1,7 @@
 export const recipesEndpoints = Object.freeze({
-  getAllRecipes: process.env.BASE_RECIPES_API_URL + "/recipes",
-  getOneRecipe: process.env.NEXT_PUBLIC_BASE_RECIPES_API_URL + "/recipes/",
-  saveRecipe: process.env.BASE_RECIPES_API_URL + "/recipes",
-  editRecipe: process.env.BASE_RECIPES_API_URL + "/recipes/",
-  deleteRecipe: process.env.BASE_RECIPES_API_URL + "/recipes/",
+  getAllRecipes: new URL(`${process.env.BASE_RECIPES_API_URL}/recipes`),
+  getOneRecipe: new URL(`${process.env.BASE_RECIPES_API_URL}/recipes/`),
+  saveRecipe: new URL(`${process.env.BASE_RECIPES_API_URL}/recipes`),
+  editRecipe: new URL(`${process.env.BASE_RECIPES_API_URL}/recipes/`),
+  deleteRecipe: new URL(`${process.env.BASE_RECIPES_API_URL}/recipes/`),
 });
